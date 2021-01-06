23370cda710fca8fd81826aeb48f8fdc72f0e055
Surasak Sangkhathat <67556099+sasurasa@users.noreply.github.com>
2021-01-07 00:00:59 +0700

    Add files via upload

---
+++ soap_chitest.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+
+#Setting Chi-square test for association between a certain factor with surgical outcome
+
+import numpy as np
+import pandas as pd
+from scipy.stats import chi2_contingency
+
+#data: python Dataframe
+#outcome: binary data
+#factor: should be binary as well
+path = input('Excel path: ')
+data = pd.read_excel(path, sheet_name='Sheet1', parse_dates = True, engine='openpyxl')
+var_b = input('Outcome to be analysed: ')
+var_a = input('Variable to be tested against outcome: ')
+
+def chi_p(data, var_b, var_a):
+	table = pd.crosstab(data[var_b], data[var_a])
+	c, p, dof, expected = chi2_contingency(table)
+	print('Data dimension: ', table.shape)
+	print('Chisquare p-value =',p)
+print('=================================================================================\n')
+chi_p(data, var_b, var_a)
+print('=================================================================================\n')
+twosub = data[[var_a, var_b]]
+var_b_list = data[var_b].unique().tolist()
+all_col_list = []
+for i in var_b_list:
+	Bi = twosub[data[var_b] == i]
+	Ci = Bi.groupby(var_a).count()
+	dict = {Ci.columns[0]:Ci.columns[0]+str(i)}
+	Ci = Ci.rename(columns=dict)
+	all_col_list.append(Ci)
+
+d = pd.concat(all_col_list, axis=1, join='inner')
+sum = d.aggregate('sum', axis = 1)
+e = pd.concat([d, sum], axis=1, join='inner')
+e = e.rename(columns={0:'horizonsum'})
+sum = e.aggregate('sum', axis = 0)
+e = e.append(sum, ignore_index=True)
+lst_a = data[var_a].unique()
+lst_a.sort()
+lst_a = lst_a.tolist()
+lst_a.append('vertisum')
+e[var_a] = lst_a
+
+cols = e.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+e = e[cols]
+
+print(e)
+print('\n')
+
+
+
+
+
+
