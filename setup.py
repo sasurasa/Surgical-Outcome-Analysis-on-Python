9a383535d151c6e0409b3cafc6ad6f6f0755ee0f
Surasak Sangkhathat <67556099+sasurasa@users.noreply.github.com>
2021-01-25 16:29:49 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='SURPY',
+version='1.0',
+description='Testing installation of Package',
+url='https://github.com/sasurasa/Surgical-Outcome-Analysis-on-Python/tree/SURG-package',
+author='Surasak Sangkhathat',
+author_email='s.sangkhathat@gmail.com',
+license='Prince of Songkla University',
+packages=['SURPY'],
+zip_safe=False)
