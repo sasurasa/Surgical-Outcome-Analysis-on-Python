72d4708a92e5c13aefc8265096622bc497f54875
Surasak Sangkhathat <67556099+sasurasa@users.noreply.github.com>
2021-01-25 15:41:16 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='SODA',
 version='1.0',
 description='Testing installation of Package',
-url='https://github.com/sasurasa/Surgical-Outcome-Analysis-on-Python/tree/SOAP-package'
+url='https://github.com/sasurasa/Surgical-Outcome-Analysis-on-Python/tree/SOAP-package',
 author='Surasak Sangkhathat',
 author_email='s.sangkhathat@gmail.com',
 license='Prince of Songkla University',
