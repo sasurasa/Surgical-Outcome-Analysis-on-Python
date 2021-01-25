e7bc5b8a1d65ace38ae3574f7e458c0a449f274a
Surasak Sangkhathat <67556099+sasurasa@users.noreply.github.com>
2021-01-25 16:32:33 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(name='SURPY',
 version='1.0',
 description='Testing installation of Package',
-url='https://github.com/sasurasa/Surgical-Outcome-Analysis-on-Python/tree/SURG-package',
+url='https://github.com/sasurasa/Surgical-Outcome-Analysis-on-Python/tree/SURPY',
 author='Surasak Sangkhathat',
 author_email='s.sangkhathat@gmail.com',
 license='Prince of Songkla University',
