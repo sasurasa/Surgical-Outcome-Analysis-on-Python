51a93b1a25124a92fe7be24099d899c0c6a30a54
Surasak Sangkhathat <67556099+sasurasa@users.noreply.github.com>
2021-01-25 15:34:18 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='SODA',
+version='1.0',
+description='Testing installation of Package',
+url='#',
+author='Surasak Sangkhathat',
+author_email='s.sangkhathat@gmail.com',
+license='Prince of Songkla University',
+packages=['SODA'],
+zip_safe=False)
