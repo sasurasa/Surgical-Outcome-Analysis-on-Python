37297ef296ef3f55fa9af8b3fc2a3133e94c66ce
Surasak Sangkhathat <67556099+sasurasa@users.noreply.github.com>
2021-03-08 17:00:43 +0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
+
+with open ("README.md") as f:
+	long_description = f.read()
 setup(name='SURPY',
-version='1.0',
-description='Testing installation of Package',
-url='https://github.com/sasurasa/Surgical-Outcome-Analysis-on-Python/tree/SURPY',
+version='1.1.4',
+description='SURPY python for surgical data analysis',
+url='https://github.com/sasurasa/Surgical-Outcome-Analysis-on-Python/tree/SURG-package',
 author='Surasak Sangkhathat',
 author_email='s.sangkhathat@gmail.com',
 license='Prince of Songkla University',
 packages=['SURPY'],
-zip_safe=False)
+zip_safe=False,
+long_description=long_description,
+    long_description_content_type='text/markdown'
+)
